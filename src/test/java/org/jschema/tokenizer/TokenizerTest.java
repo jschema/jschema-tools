fa772cdfd0d2a06f0f01311382472233f2403a6f
Sachin Patel <me@gizmosachin.com>
2016-01-14 19:17:37 -0800

    Parser work

--- src/test/java/org/jschema/tokenizer/TokenizerTest.java
+++ src/test/java/org/jschema/tokenizer/TokenizerTest.java
@@ -11,6 +11,13 @@ public class TokenizerTest
 {
 
   @Test
+  public void parseTest() {
+    Parser parser = new Parser();
+    String json = "{ \"glossary\": { \"title\": \"example glossary\", \"GlossDiv\": { \"title\": \"S\", \"GlossList\": { \"GlossEntry\": { \"ID\": \"SGML\", \"SortAs\": \"SGML\", \"GlossTerm\": \"Standard Generalized Markup Language\", \"Acronym\": \"SGML\", \"Abbrev\": \"ISO 8879:1986\", \"GlossDef\": { \"para\": \"A meta-markup language, used to create markup languages such as DocBook.\", \"GlossSeeAlso\": [\"GML\", \"XML\"] }, \"GlossSee\": \"markup\" } } } } }";
+    parser.parse(json);
+  }
+
+  @Test
   public void bootstrapTest()
   {
     // basic constant
