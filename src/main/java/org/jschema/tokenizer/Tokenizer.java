13586618cdb1b152f4c63617ff27cf9e5887412c
RemingtonMaxwell <remingtonmaxwell@att.net>
2016-01-18 03:01:27 -0800

    Punctuation, Constants,and Numbers working except for exponents

--- src/main/java/org/jschema/tokenizer/Tokenizer.java
+++ src/main/java/org/jschema/tokenizer/Tokenizer.java
@@ -2,18 +2,251 @@ package org.jschema.tokenizer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import static org.jschema.tokenizer.Token.TokenType.*;
 
 public class Tokenizer
 {
   private String _string;
+  private char[] _chars;
+  private int _offset;
+  private int _line;
+  private int _column;
 
   public Tokenizer( String string )
   {
     _string = string;
   }
 
+  //========================================================================================
+  //  Main tokenization loop
+  //========================================================================================
+
   public List<Token> tokenize()
   {
-    return new ArrayList<Token>();
-  };
+    ArrayList<Token> tokens = new ArrayList<Token>();
+
+    _chars = _string.toCharArray();
+    _offset = 0;
+    _line = 1;
+    _column = 0;
+
+    while(moreChars()) {
+      eatWhiteSpace(); // eat leading whitespace
+
+      if(!moreChars()) break; // if we got to the end of the string, exit
+
+      Token string = consumeString();
+      if(string != null)
+      {
+        tokens.add( string );
+        continue;
+      }
+
+      Token number = consumeNumber();
+      if(number != null)
+      {
+        tokens.add( number );
+        continue;
+      }
+
+      Token punctuation = consumePunctuation();
+      if(punctuation != null)
+      {
+        tokens.add( punctuation );
+        continue;
+      }
+
+      Token constant = consumeConstant();
+      if(constant != null)
+      {
+        tokens.add( constant );
+        continue;
+      }
+        System.out.println(currentChar());
+      // unrecognized token, add error token
+      tokens.add( newToken( ERROR, ">> BAD TOKEN : " + currentChar() ) );
+      bumpOffset( 1 );
+    }
+
+    return tokens;
+  }
+
+  //========================================================================================
+  //  Tokenization type methods
+  //========================================================================================
+
+  private Token consumeString()
+  {
+    //TODO - implement
+    return null;
+  }
+
+    //needs to work for integers and decimals
+  private Token consumeNumber()
+  {
+      String token="";
+      //whether there is a decimal or not
+      int isDecimal=0;
+      int offset=0;
+      while( _offset+offset < _chars.length && (Character.isDigit(_chars[_offset+offset])||_chars[_offset+offset]=='.'||_chars[_offset+offset]=='/')){
+          token +=_chars[_offset+offset];
+          //don't need to check if number after decimal->valid to have 20. in javascript
+          if(_chars[_offset+offset]=='.'){
+              isDecimal++;
+          }
+          offset++;
+      }
+      if(!token.equals("")){
+          //in case multiple decimals appear from invalid input
+          Token t;
+          //invalid number token
+          if(isDecimal>1){
+              t= newToken( ERROR, ">> BAD TOKEN : " + token);
+              //make sure that fraction isn't null before or after it
+          }else {
+              t = newToken(NUMBER, token);
+          }
+          bumpOffset(offset);
+          return t;
+      }
+    return null;
+  }
+
+  private Token consumePunctuation()
+  {
+      //Square brackets, curly brackets, comma, colon
+    switch(_chars[_offset]){
+        case '{': return createPuncToken("{");
+        case '}': return createPuncToken("}");
+        case '[': return createPuncToken("[");
+        case ']': return createPuncToken("]");
+        case ',': return createPuncToken(",");
+        case ':': return createPuncToken(":");
+        default: break;
+    }
+    return null;
+  }
+
+    private Token createPuncToken(String token){
+        Token t = newToken( PUNCTUATION, token );
+        bumpOffset(1);
+        return t;
+    }
+    //handles case where there is a partial match to "true" or "false"
+    private Token invalidConstant(int offset) {
+        if (((_offset + offset) < _chars.length) && !(Character.toString(_chars[_offset + offset]).equals(" "))) {
+            String token = "true";
+            int offsetNew = offset;
+            while (_offset + offsetNew < _chars.length) {
+                token += _chars[_offset + offsetNew];
+                offsetNew++;
+            }
+            Token t = newToken(ERROR, ">> BAD TOKEN : " + token);
+            bumpOffset(offsetNew);
+            return t;
+        }else{
+            return null;
+        }
+    }
+  private Token consumeConstant()
+  {
+    if( match( 't', 'r', 'u', 'e' ) )
+    {
+        Token t=invalidConstant(4);
+        if(t!=null){
+            return t;
+        }else {
+            t = newToken(CONSTANT, "true");
+            bumpOffset(4);
+            return t;
+        }
+    }
+    if( match( 'f', 'a', 'l', 's', 'e' ) )
+    {
+        Token t=invalidConstant(5);
+        if(t!=null){
+            return t;
+        }else {
+            t = newToken(CONSTANT, "false");
+            bumpOffset(5);
+            return t;
+        }
+    }
+    if( match( 'n', 'u', 'l', 'l' ) )
+    {
+        Token t=invalidConstant(4);
+        if(t!=null){
+            return t;
+        }else {
+            t = newToken(CONSTANT, "null");
+            bumpOffset(4);
+            return t;
+        }
+    }
+    return null;
+  }
+
+  //========================================================================================
+  //  Utility methods
+  //========================================================================================
+
+  private void bumpOffset( int amt )
+  {
+    _offset += amt;
+  }
+
+  private Token newToken( Token.TokenType type, String tokenValue )
+  {
+    return new Token( type, tokenValue, _line, _column, _offset + 1 );
+  }
+
+  private boolean match( char... charArray)
+  {
+    for( int i = 0; i < charArray.length; i++ )
+    {
+      if( !peekAndMatch( i, charArray[i] ))
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean peekAndMatch( int i, char toMatch )
+  {
+    if( _offset + i < _chars.length )
+    {
+      return _chars[_offset + i] == toMatch;
+    } else {
+      return false;
+    }
+  }
+
+  private void eatWhiteSpace()
+  {
+    //while there exists more characters and the current character is white space
+    while( moreChars() && Character.isWhitespace( currentChar() ) )
+    {
+      char c = currentChar();
+      if( c == '\n' ) // if we are at a newline character, bump the line number and reset the column
+      {
+        _line++;
+        _column = 0;
+      }
+      _offset = _offset + 1; // bump offset
+      _column = _column + 1; // bump column
+    }
+  }
+
+  private char currentChar()
+  {
+    return _chars[_offset];
+  }
+
+  private boolean moreChars()
+  {
+    return _offset < _chars.length;
+  }
 }
--- src/test/java/org/jschema/tokenizer/TokenizerTest.java
+++ src/test/java/org/jschema/tokenizer/TokenizerTest.java
@@ -5,19 +5,154 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.jschema.tokenizer.Token.TokenType.CONSTANT;
+import static org.jschema.tokenizer.Token.TokenType.PUNCTUATION;
+import static org.jschema.tokenizer.Token.TokenType.NUMBER;
+import static org.jschema.tokenizer.Token.TokenType.STRING;
+import static org.jschema.tokenizer.Token.TokenType.ERROR;
+
 public class TokenizerTest
 {
 
   @Test
   public void bootstrapTest()
   {
-    List<Token> tokens = tok( "true" );
-    Assert.assertEquals( 1, tokens.size() );
+    //basic constant
+    List<Token> tokens = tokenize( "true" );
+    assertTokensAre( tokens, token( CONSTANT, "true" ) );
+
+    // leading whitespace
+    tokens = tokenize( "   true" );
+    assertTokensAre( tokens, token( CONSTANT, "true" ) );
+
+    // trailing whitespace
+    tokens = tokenize( "true   " );
+    assertTokensAre( tokens, token( CONSTANT, "true" ) );
+
+    // trailing whitespace
+    tokens = tokenize( "true   " );
+    assertTokensAre( tokens, token( CONSTANT, "true" ) );
+
+    // two tokens whitespace
+    tokens = tokenize( "true   false" );
+    assertTokensAre( tokens, token( CONSTANT, "true" ), token( CONSTANT, "false" ) );
+
+    // invalid constant (constant with typo)
+    tokens = tokenize( "truel" );
+    assertTokensAre( tokens,token(ERROR,">> BAD TOKEN : truel"));
+
+    //braces
+    tokens = tokenize( "[]" );
+    assertTokensAre( tokens, token( PUNCTUATION, "[" ), token( PUNCTUATION, "]" ) );
+
+    //single curly brace
+    tokens = tokenize( "{" );
+    assertTokensAre(tokens,token(PUNCTUATION,"{"));
+
+    //curly braces with white space
+    tokens = tokenize( "{   }" );
+    assertTokensAre( tokens, token( PUNCTUATION, "{" ), token( PUNCTUATION, "}" ) );
+
+    //comma
+    tokens = tokenize( "," );
+    assertTokensAre( tokens, token( PUNCTUATION, "," ));
+
+    //colon
+    tokens = tokenize( ":" );
+    assertTokensAre( tokens, token( PUNCTUATION, ":" ));
+
+    //number by itself
+    tokens = tokenize( "1234" );
+    assertTokensAre( tokens, token( NUMBER, "1234" ));
+
+    //multiple numbers
+    tokens = tokenize( "1 2 3" );
+    assertTokensAre( tokens, token(NUMBER,"1"),token( NUMBER, "2" ), token(NUMBER,"3"));
+
+    //decimal number
+    tokens = tokenize( "1.23" );
+    assertTokensAre( tokens, token(NUMBER,"1.23"));
+
+    //invalid decimal number
+    tokens = tokenize( "1..23" );
+    assertTokensAre( tokens, token(ERROR,">> BAD TOKEN : 1..23"));
+
+    //decimal number
+    tokens = tokenize( ".23" );
+    assertTokensAre( tokens, token(NUMBER,".23"));
+
+    //multiple fractions
+    tokens = tokenize( ".23 .56" );
+    assertTokensAre( tokens, token(NUMBER,".23"),token(NUMBER,".56"));
+    
+
   }
 
-  private List<Token> tok( String aTrue )
+  //========================================================================================
+  // Test Helpers
+  //========================================================================================
+
+  private void assertTokensAre( List<Token> tokens, Token... matches )
   {
-    return new Tokenizer( aTrue ).tokenize();
+    if( tokens.size() != matches.length )
+    {
+      Assert.fail( "Did not find " + matches.length + " tokens: " + tokens);
+    }
+    for( int i = 0; i < matches.length; i++ )
+    {
+      assertTokenMatches( matches[i], tokens.get( i ) );
+    }
+  }
+
+  private void assertTokenMatches( Token match, Token token )
+  {
+    Assert.assertEquals( match.getTokenType(), token.getTokenType() );
+    if( match.getTokenValue() != null )
+    {
+      Assert.assertEquals( match.getTokenValue(), token.getTokenValue() );
+    }
+    if( match.getLineNumber() > 0 )
+    {
+      Assert.assertEquals( match.getLineNumber(), token.getLineNumber() );
+    }
+    if( match.getOffest() > 0 )
+    {
+      Assert.assertEquals( match.getOffest(), token.getOffest() );
+    }
+    if( match.getColumn() > 0 )
+    {
+      Assert.assertEquals( match.getColumn(), token.getColumn() );
+    }
+  }
+
+  private Token token(Token.TokenType type)
+  {
+    return token( type, null );
+  }
+
+  private Token token(Token.TokenType type, String value)
+  {
+    return token( type, value, -1);
+  }
+
+  private Token token(Token.TokenType type, String value, int offset)
+  {
+    return token( type, value, offset, -1);
+  }
+
+  private Token token(Token.TokenType type, String value, int offset, int line)
+  {
+    return token( type, value, offset, line, -1);
+  }
+
+  private Token token(Token.TokenType type, String value, int offset, int line, int col)
+  {
+    return new Token( type, value, line, col, offset );
+  }
+
+  private List<Token> tokenize( String str )
+  {
+    return new Tokenizer( str ).tokenize();
   }
 
 }
