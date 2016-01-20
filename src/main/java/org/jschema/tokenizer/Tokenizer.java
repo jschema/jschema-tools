757e035d0433b10ae29cf7ccd398c21012be89bb
RemingtonMaxwell <remingtonmaxwell@att.net>
2016-01-19 18:46:52 -0800

    working on string

--- src/main/java/org/jschema/tokenizer/Tokenizer.java
+++ src/main/java/org/jschema/tokenizer/Tokenizer.java
@@ -2,6 +2,7 @@ package org.jschema.tokenizer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static org.jschema.tokenizer.Token.TokenType.*;
 
@@ -13,7 +14,7 @@ public class Tokenizer
   private int _line;
   private int _column;
 
-  public Tokenizer( String string )
+  public Tokenizer(String string )
   {
     _string = string;
   }
@@ -36,7 +37,7 @@ public class Tokenizer
 
       if(!moreChars()) break; // if we got to the end of the string, exit
 
-      Token string = consumeString();
+        Token string = consumeString();
       if(string != null)
       {
         tokens.add( string );
@@ -63,7 +64,7 @@ public class Tokenizer
         tokens.add( constant );
         continue;
       }
-        System.out.println(currentChar());
+        //System.out.println(currentChar());
       // unrecognized token, add error token
       tokens.add( newToken( ERROR, ">> BAD TOKEN : " + currentChar() ) );
       bumpOffset( 1 );
@@ -78,8 +79,44 @@ public class Tokenizer
 
   private Token consumeString()
   {
-    //TODO - implement
-    return null;
+      String tok="";
+
+
+      int newOffset=0;
+      //check if starts with quote
+    if(_chars[_offset]=='"'){
+        tok+=_chars[_offset+newOffset];
+        newOffset++;
+        //go until a space is found
+        //should cover case with escaped quotes inside string
+        while(_offset+newOffset<_chars.length
+                &&_chars[_offset+newOffset]!=':' &&_chars[_offset+newOffset]!=','){
+            tok+=_chars[_offset+newOffset];
+            //escaped " case
+            if(_chars[_offset+newOffset]=='\\' && _offset+newOffset+1<_chars.length &&_chars[_offset+newOffset]=='"'){
+                newOffset++;
+            }
+            newOffset++;
+            //break if last " is found and next letter is space
+            if(_chars[_offset+newOffset-1]=='"' &&_offset+newOffset<_chars.length && !Character.toString(_chars[_offset+newOffset]).equals(" ")) {
+                newOffset++;
+                break;
+            }
+        }
+
+        //check to make sure ends in quote and is not a single quote
+        if(!tok.substring(tok.length()-1).equals("\"") || tok.length()==1){
+            bumpOffset(newOffset);
+            return newToken(ERROR,">> BAD TOKEN : " + tok);
+        }else{
+            bumpOffset(newOffset);
+
+            return newToken(STRING,tok);
+        }
+    }
+
+      return null;
+
   }
 
     //needs to work for integers and decimals
@@ -88,25 +125,52 @@ public class Tokenizer
       String token="";
       //whether there is a decimal or not
       int isDecimal=0;
+      int decOffset=0;
+      //whether there is an exponent or not
+      int isExp=0;
+      int expOffset=0;
       int offset=0;
-      while( _offset+offset < _chars.length && (Character.isDigit(_chars[_offset+offset])||_chars[_offset+offset]=='.'||_chars[_offset+offset]=='/')){
+      //for negative numbers;
+      if(_chars[_offset+offset]=='-') {
+          token +=_chars[_offset+offset];
+          offset++;
+      }
+      while( _offset+offset < _chars.length && (Character.isDigit(_chars[_offset+offset])||_chars[_offset+offset]=='.'
+              ||((_chars[_offset+offset]=='e'||_chars[_offset+offset]=='E')&& offset!=0))){
           token +=_chars[_offset+offset];
           //don't need to check if number after decimal->valid to have 20. in javascript
           if(_chars[_offset+offset]=='.'){
               isDecimal++;
+              decOffset=offset;
+              //
+          }else if (_chars[_offset+offset]=='E'||_chars[_offset+offset]=='e'){
+              isExp++;
+              expOffset=offset;
+              //for negative exponents
+              if(_offset+offset+1 < _chars.length &&_chars[_offset+offset+1]=='-'){
+                  token +=_chars[_offset+offset+1];
+                  offset++;
+              }
           }
           offset++;
       }
-      if(!token.equals("")){
+      if(!token.equals("")) {
+          //System.out.println(token);
           //in case multiple decimals appear from invalid input
           Token t;
-          //invalid number token
-          if(isDecimal>1){
-              t= newToken( ERROR, ">> BAD TOKEN : " + token);
-              //make sure that fraction isn't null before or after it
-          }else {
-              t = newToken(NUMBER, token);
+          //invalid number token, more than one '.', 'e', or a '.e'
+          if (isDecimal > 1 || isExp > 1 || (isDecimal == 1 && isExp == 1)) {
+               t = newToken(ERROR, ">> BAD TOKEN : " + token);
+          }else if (isExp==1){
+               t=checkValidExp(expOffset, token,false);
+          }else if(isDecimal==1) {
+              //System.out.println(token+" "+decOffset);
+
+              t = checkValidExp(decOffset, token,true);
+          }else{
+              t=newToken(NUMBER,token);
           }
+
           bumpOffset(offset);
           return t;
       }
@@ -128,27 +192,7 @@ public class Tokenizer
     return null;
   }
 
-    private Token createPuncToken(String token){
-        Token t = newToken( PUNCTUATION, token );
-        bumpOffset(1);
-        return t;
-    }
-    //handles case where there is a partial match to "true" or "false"
-    private Token invalidConstant(int offset) {
-        if (((_offset + offset) < _chars.length) && !(Character.toString(_chars[_offset + offset]).equals(" "))) {
-            String token = "true";
-            int offsetNew = offset;
-            while (_offset + offsetNew < _chars.length) {
-                token += _chars[_offset + offsetNew];
-                offsetNew++;
-            }
-            Token t = newToken(ERROR, ">> BAD TOKEN : " + token);
-            bumpOffset(offsetNew);
-            return t;
-        }else{
-            return null;
-        }
-    }
+
   private Token consumeConstant()
   {
     if( match( 't', 'r', 'u', 'e' ) )
@@ -248,4 +292,63 @@ public class Tokenizer
   {
     return _offset < _chars.length;
   }
+    //creates tokens for punctuation
+    private Token createPuncToken(String token){
+        Token t = newToken( PUNCTUATION, token );
+        bumpOffset(1);
+        return t;
+    }
+    //handles case where there is a partial match to "true" or "false"
+    private Token invalidConstant(int offset) {
+        if (((_offset + offset) < _chars.length) && _chars[_offset + offset]!=','
+                && !(Character.toString(_chars[_offset + offset]).equals(" "))) {
+            String token = "true";
+            int offsetNew = offset;
+            while (_offset + offsetNew < _chars.length && _chars[_offset + offsetNew]!=','
+                    &&!(Character.toString(_chars[_offset + offsetNew]).equals(" "))) {
+                token += _chars[_offset + offsetNew];
+                offsetNew++;
+            }
+            Token t = newToken(ERROR, ">> BAD TOKEN : " + token);
+            bumpOffset(offsetNew);
+            return t;
+        }else{
+            return null;
+        }
+    }
+    //checks exponents to make sure they are valid
+    private Token checkValidExp(int offset, String tok, boolean isDec) {
+        Token t=newToken(NUMBER, tok);;
+        String regex = "-?[0-9]+";
+        //check if number before e
+        if(!(isDec && tok.substring(0,offset).equals(""))) {
+            //make sure isn't small decimal like .23
+            if ((offset - 1 < 0) || !(tok.substring(0, offset).matches(regex))) {
+                t = newToken(ERROR, ">> BAD TOKEN : " + tok);
+
+                //check if number or negative sign after e
+                //TODO need to check more for validity
+            } else {
+                //get anything after token
+                int offsetAfter = 0;
+                String badTok = tok;
+                //System.out.println("tok is" +tok.length());
+                //get any bad input after exponent
+                while (tok.length() + _offset+ offsetAfter < _chars.length &&
+                        !Character.toString(_chars[_offset+tok.length() + offsetAfter]).equals(" ")&&
+                        _chars[_offset+tok.length() + offsetAfter]!=',') {
+                    badTok += _chars[tok.length() + offsetAfter];
+                    offsetAfter++;
+                }
+                bumpOffset(offsetAfter);
+                if (offsetAfter > 0) {
+                    t = newToken(ERROR, ">> BAD TOKEN : " + badTok);
+                } else {
+                    t = newToken(NUMBER, tok);
+                }
+            }
+        }
+        return t;
+    }
+
 }
