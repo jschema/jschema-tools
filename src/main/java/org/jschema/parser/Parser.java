a0e51098a2e847ab4df67dadcb83c096b883bdc0
RemingtonMaxwell <remingtonmaxwell@att.net>
2016-02-07 19:33:10 -0800

    Adding object tests with nested arrays

--- src/main/java/org/jschema/parser/Parser.java
+++ src/main/java/org/jschema/parser/Parser.java
@@ -4,7 +4,6 @@ import org.jschema.parser.Token.TokenType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import static org.jschema.parser.Token.TokenType.*;
 
 public class Parser
@@ -49,6 +48,25 @@ public class Parser
       nextToken();
       return parseArray();
     }
+      // parse arrays
+      if( match( RSQUARE ) )
+      {
+          System.out.println("test");
+          return _currentToken.getTokenValue();
+      }
+
+
+
+      if(match (COMMA)){
+          String tokenValue = _currentToken.getTokenValue();
+          nextToken();
+          return tokenValue;
+      }
+      if(match (COLON)){
+          String tokenValue = _currentToken.getTokenValue();
+          nextToken();
+          return tokenValue;
+      }
 
     // parse literals (e.g. true, false, strings, numbers)
     if( match( STRING ) )
@@ -58,42 +76,192 @@ public class Parser
       return tokenValue;
     }
 
+      if (match (TRUE)){
+          nextToken();
+          return true;
+      }
+      if (match (FALSE)){
+          nextToken();
+          return false;
+      }
+      if (match (NULL)){
+          nextToken();
+          return null;
+      }
+
+      if (match(NUMBER)){
+          double num=_currentToken.getTokenNumberValue();
+          //check if is an int
+          if(num % 1==0 && !_currentToken.getTokenValue().contains(".")){
+              nextToken();
+              return ((int)num);
+          }
+          nextToken();
+          return num;
+
+      }
+  if(match(ERROR)){
+      return error();
+  }
+
     //TODO implement other literals
 
     return error();
   }
 
-  public Object parseObject()
+  /*public Object parseObject()
   {
     //TODO implement, return a map of name/value pairs, and Error if an error is detected
     //                pass the map into parseMember to populate
-    HashMap<String, Object> map = new HashMap<>();
-    if( match( RCURLY ) )
-    {
+      HashMap<String, Object> map = new HashMap<>();
+      //for empty object
+      if( match( RCURLY ) )
+      {
+          nextToken();
+          return map;
+      }
+      map=(HashMap<String, Object>) parseMember(map);
+      System.out.println("map is "+map.toString());
+      System.out.println("current token is "+_currentToken.getTokenValue());
+      //make sure it matches right curly brace
+      //keep going if there is a comma
+      while(!match(RCURLY)) {
+          nextToken();
+          if(match(LCURLY)) {
+              nextToken();
+              map = (HashMap<String, Object>) parseMember(map);
+          }else{
+              return error();
+          }
+      }
+      System.out.println("final map is "+map.toString());
+    return map;
+  }*/
+  public Object parseObject()
+  {
+      //TODO implement, return a map of name/value pairs, and Error if an error is detected
+      //                pass the map into parseMember to populate
+      HashMap<String, Object> map = new HashMap<>();
+      //for empty object
+      if( match( RCURLY ) )
+      {
+          nextToken();
+          return map;
+      }
+     // map=(HashMap<String, Object>) parseMember(map);
+      //System.out.println("current token is "+_currentToken.getTokenValue());
+      //make sure it matches right curly brace
+      //keep going if there is a comma
+      while(!match(RCURLY)) {
+
+          map = (HashMap<String, Object>) parseMember(map);
+          System.out.println("map is "+map.toString());
+          /*if(match(LCURLY)) {
+              nextToken();
+              map = (HashMap<String, Object>) parseMember(map);
+          }else{
+              return error();
+          }*/
+          if(!match(RCURLY)) {
+              nextToken();
+          }
+      }
       nextToken();
+      System.out.println("final map is "+map.toString());
       return map;
-    }
-    else
-    {
-      return error();
-    }
   }
 
   private Object parseMember( HashMap map )
   {
-    //TODO implement, parse the key and value, return the map if it is good, Error otherwise.
-    return map;
+      System.out.println(_currentToken.getTokenValue());
+
+      //get the key
+      String key=_currentToken.getTokenValue();
+      System.out.println("key is "+key);
+      Object val;
+      //now check that colon is separator
+      nextToken();
+      if(!match(COLON)){
+          error();
+          nextToken();
+          return map;
+      }else{
+          //now get value
+          nextToken();
+          System.out.println("next tok is "+_currentToken.getTokenValue());
+          val=parseValue();
+          System.out.println("val is "+val.toString());
+          map.put(key,val);
+
+      }
+      //check that last curly brace matches
+      if( match( RCURLY ) )
+      {
+          //nextToken();
+          System.out.println("map is now"+map.toString());
+          return map;
+      }
+      else
+      {
+          return error();
+      }
+      //TODO implement, parse the key and value, return the map if it is good, Error otherwise.
   }
+
 
   public Object parseArray()
   {
-    //TODO implement, parse the elements inline, return Error if any element is error
-    return new ArrayList();
+      ArrayList list=new ArrayList();
+      //for empty arrays
+      if(match (RSQUARE)) {
+          nextToken();
+          return list;
+      }
+
+      while (!match(RSQUARE)) {
+          System.out.println("in parse array "+_currentToken.getTokenValue());
+          try {
+              //first get item
+              Object val=parseValue();
+              System.out.println("value returned is "+val.toString());
+
+              //if not comma, add to list since
+              if(!val.equals(",")){
+                  list.add(val);
+              }else{
+                  //return error if not valid, already on next token
+                  //so don't need to switch index
+                  error();
+                  return new ArrayList();
+              }
+              //next token must be comma or right square
+              //if not, print error, go to next token
+              if(!(match(COMMA)||match(RSQUARE))){
+                  error();
+                  nextToken();
+                  return new ArrayList();
+              }
+              //if a comma, move to next token
+              //otherwise do not to stop while loop properly on RSQUARE
+              if(match(COMMA)) {
+                  nextToken();
+              }
+          }catch (Exception e){
+              error();
+              nextToken();
+              return list;
+          }
+          //TODO implement, parse the elements inline, return Error if any element is error
+      }
+      nextToken();
+      System.out.println("array is "+list.toString());
+      return list;
   }
 
   //=================================================================================
   //  Tokenizer helpers
   //=================================================================================
+
   private void nextToken()
   {
     _currentToken = _tokenizer.next();
--- src/test/java/org/jschema/parser/ParserTest.java
+++ src/test/java/org/jschema/parser/ParserTest.java
@@ -3,9 +3,11 @@ package org.jschema.parser;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
@@ -19,15 +21,54 @@ public class ParserTest
     assertEquals( new HashMap(), parse( "{}" ) );
     HashMap map1 = new HashMap();
     map1.put( "foo", "bar" );
-    assertEquals( map1, parse( "{\"foo\":\"bar\"}" ) );
-    //TODO add more
+      HashMap map5=new HashMap();
+    map5.put("bar","baz");
+      ArrayList temp=new ArrayList();
+      temp.add(map1);
+      temp.add(map5);
+    System.out.println(map1.toString());
+    assertEquals( temp, parse( "[{\"foo\":\"bar\"},{\"bar\":\"baz\"}]" ) );
+    HashMap map2 = new HashMap();
+      HashMap map3=new HashMap();
+      ArrayList testArray=new ArrayList();
+      testArray.add(1);
+      testArray.add(2);
+      testArray.add(3);
+    map2.put( "foo2", testArray );
+    assertEquals( map2, parse( "{\"foo2\":[1,2,3]}" ) );
+      HashMap map4=new HashMap();
+      ArrayList testArray2=new ArrayList();
+      testArray2.add(temp);
+      testArray2.add(map2);
+      map4.put("array",testArray2);
+      System.out.println("crazy map is "+map4.toString());
+      assertEquals( map4, parse( "{\"array\":[[{\"foo\":\"bar\"},{\"bar\":\"baz\"}],{\"foo2\":[1,2,3]}]}"));//,{\"bar\":\"baz\"},{\"foo\":[1,2,3]},{\"test\":{\"foo\":\"bar\"},{\"bar\":\"baz\"}}]" ) );
+      //TODO add more
   }
 
+  @Test
+  public void testParsePunc() {
+    assertEquals( ",", parse( "," ) );
+  }
   @Test
   public void testParseArray()
   {
     assertEquals( new ArrayList(), parse( "[]" ) );
     assertEquals( Arrays.asList( "foo", "bar" ), parse( "[\"foo\", \"bar\"]" ) );
+    int[][] nestedArray = {{1,2,3},{4,5,6}};
+    ArrayList nestArray=new ArrayList();
+    ArrayList nest1= new ArrayList();
+    nest1.add(1);
+    nest1.add(2);
+    nest1.add(3);
+    ArrayList nest2= new ArrayList();
+    nest2.add(4);
+    nest2.add(5);
+    nest2.add(6);
+    nestArray.add(nest1);
+    nestArray.add(nest2);
+    assertEquals(Arrays.asList(1,2,3), parse( "[1,2,3]" ) );
+    assertEquals(nestArray, parse( "[[1,2,3],[4,5,6]]" ));
     //TODO add more
   }
 
