5ce20b3b617359a3f29fc8e32116ffa0d8de5557
RemingtonMaxwell <remingtonmaxwell@att.net>
2016-02-09 17:47:57 -0800

    passing all tests

--- src/main/java/org/jschema/parser/Parser.java
+++ src/main/java/org/jschema/parser/Parser.java
@@ -4,252 +4,204 @@ import org.jschema.parser.Token.TokenType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
 import static org.jschema.parser.Token.TokenType.*;
 
-public class Parser
-{
+public class Parser {
 
-  private final Tokenizer _tokenizer;
-  private Token _currentToken;
+    private final Tokenizer _tokenizer;
+    private Token _currentToken;
 
-  public Parser( String src )
-  {
-    _tokenizer = new Tokenizer( src );
-    nextToken();
-  }
-
-  //=================================================================================
-  //  JSON Grammar
-  //=================================================================================
-
-  public Object parse() {
-    Object value = parseValue();
-    if( match( EOF ) )
-    {
-      return value;
-    }
-    else
-    {
-      return error();
-    }
-  }
-
-  public Object parseValue()
-  {
-    if( match( LCURLY ) )
-    {
-      nextToken();
-      return parseObject();
+    public Parser(String src) {
+        _tokenizer = new Tokenizer(src);
+        nextToken();
     }
 
-    // parse arrays
-    if( match( LSQUARE ) )
-    {
-      nextToken();
-      return parseArray();
-    }
-      // parse arrays
-      if( match( RSQUARE ) )
-      {
-          return _currentToken.getTokenValue();
-      }
+    //=================================================================================
+    //  JSON Grammar
+    //=================================================================================
 
-
-
-      if(match (COMMA)){
-          String tokenValue = _currentToken.getTokenValue();
-          nextToken();
-          return tokenValue;
-      }
-      if(match (COLON)){
-          String tokenValue = _currentToken.getTokenValue();
-          nextToken();
-          return tokenValue;
-      }
-
-    // parse literals (e.g. true, false, strings, numbers)
-    if( match( STRING ) )
-    {
-      String tokenValue = _currentToken.getTokenValue();
-      nextToken();
-      return tokenValue;
+    public Object parse() {
+        Object value = parseValue();
+        if (match(EOF)) {
+            return value;
+        } else {
+            return error();
+        }
     }
 
-      if (match (TRUE)){
-          nextToken();
-          return true;
-      }
-      if (match (FALSE)){
-          nextToken();
-          return false;
-      }
-      if (match (NULL)){
-          nextToken();
-          System.out.println("here");
-          return null;
-      }
+    public Object parseValue() {
+        //parse objects
+        if (match(LCURLY)) {
+            nextToken();
+            return parseObject();
+        }
 
-      if (match(NUMBER)){
-          double num=_currentToken.getTokenNumberValue();
-          System.out.println(num);
-          //check if is an int
-          if(!(_currentToken.getTokenValue().contains(".") ||_currentToken.getTokenValue().contains("e")||_currentToken.getTokenValue().contains("E"))){
-              nextToken();
-              return ((int)num);
-          }
-          nextToken();
-          return num;
+        // parse arrays
+        if (match(LSQUARE)) {
+            nextToken();
+            return parseArray();
+        }
 
-      }
-  if(match(ERROR)){
-      return error();
-  }
+        // parse literals (e.g. true, false, strings, numbers)
+        if (match(STRING)) {
+            String tokenValue = _currentToken.getTokenValue();
+            nextToken();
+            return tokenValue;
+        }
 
-    //TODO implement other literals
+        if (match(TRUE)) {
+            nextToken();
+            return true;
+        }
+        if (match(FALSE)) {
+            nextToken();
+            return false;
+        }
+        if (match(NULL)) {
+            nextToken();
+            return null;
+        }
 
-    return error();
-  }
+        if (match(NUMBER)) {
+            double num = _currentToken.getTokenNumberValue();
+            //check if is an int
+            if (!isExponent()) {
+                nextToken();
+                return ((int) num);
+            }
+            nextToken();
+            return num;
 
-
-  public Object parseObject()
-  {
-      //TODO implement, return a map of name/value pairs, and Error if an error is detected
-      //                pass the map into parseMember to populate
-      HashMap<String, Object> map = new HashMap<>();
-      //for empty object
-      if( match( RCURLY ) )
-      {
-          nextToken();
-          return map;
-      }
-        int rBrace=0;
-      //make sure it matches right curly brace
-      //keep going if there is a comma
-      while(!match(RCURLY) && !match(EOF)) {
-          map = (HashMap<String, Object>) parseMember(map);
-          if(!match(RCURLY) || match(COMMA)) {
-              nextToken();
-          }
-          if(match(RCURLY)){
-              rBrace=1;
-          }
-      }
-      nextToken();
-      if(rBrace!=1){
-          return error();
-      }
-      return map;
-  }
-
-  private Object parseMember( HashMap map )
-  {
-
-      //get the key
-      String key=_currentToken.getTokenValue();
-      System.out.println("key is"+key);
-      Object val;
-      //now check that colon is separator
-      nextToken();
-      if(!match(COLON)){
-          error();
-          nextToken();
-          return map;
-      }else{
-          //now get value
-          nextToken();
-          val=parseValue();
-          System.out.println("val added is"+val);
-          map.put(key,val);
-
-      }
-
-      if (match(COMMA)){
-          nextToken();
-          return parseMember(map);
-      } else if (match (RCURLY)){
-          return map;
-      }
-      else
-      {
-          return error();
-      }
-      //TODO implement, parse the key and value, return the map if it is good, Error otherwise.
-  }
+        }
+        if (match(ERROR)) {
+            return error();
+        }
 
 
-  public Object parseArray()
-  {
-      ArrayList list=new ArrayList();
-      //for empty arrays
-      if(match (RSQUARE)) {
-          nextToken();
-          return list;
-      }
-      int rBrace=0;
-      while (!match(RSQUARE) && !match(EOF)) {
-          try {
-              //first get item
-              Object val=parseValue();
-                System.out.println("val is"+val);
-              //if not comma, add to list since
-              if(val==null || !val.equals(",")){
-                  System.out.println("adding");
-                  list.add(val);
-              }else{
-                  //return error if not valid, already on next token
-                  //so don't need to switch index
-                  System.out.println("error");
-                  error();
-                  return new ArrayList();
-              }
-              //next token must be comma or right square
-              //if not, print error, go to next token
-              if(!(match(COMMA)||match(RSQUARE))){
-                  error();
-                  nextToken();
-                  return new ArrayList();
-              }
-              //if a comma, move to next token
-              //otherwise do not to stop while loop properly on RSQUARE
-              if(match(COMMA)) {
-                  nextToken();
-              }
-              if(match(RSQUARE)){
-                  System.out.println("here");
-                  rBrace=1;
-              }
-          }catch (Exception e){
-              error();
-              System.out.println("error");
-              nextToken();
-              return list;
-          }
+        return error();
+    }
 
-          //TODO implement, parse the elements inline, return Error if any element is error
-      }
-      nextToken();
-      if(rBrace!=1){
-          return error();
-      }
-      return list;
-  }
 
-  //=================================================================================
-  //  Tokenizer helpers
-  //=================================================================================
+    public Object parseObject() {
+        //pass the map into parseMember to populate
+        HashMap<String, Object> map = new HashMap<>();
+        //for empty object
+        if (match(RCURLY)) {
+            nextToken();
+            return map;
+        }
+        int rBrace = 0;
+        //keep going if there is a comma
+        while (!match(RCURLY) && !match(EOF)) {
+            try {
+                map = (HashMap<String, Object>) parseMember(map);
 
-  private void nextToken()
-  {
-    _currentToken = _tokenizer.next();
-  }
+                if (!match(RCURLY) || match(COMMA)) {
+                    nextToken();
+                }
+                if (match(RCURLY)) {
+                    rBrace = 1;
+                }
+                //if a map isn't returned, an error must be returned
+            } catch (ClassCastException e) {
+                return error();
+            }
+        }
+        nextToken();
+        //if no matching right brace, return error
+        if (rBrace != 1) {
+            return error();
+        }
+        return map;
+    }
 
-  private boolean match( TokenType type )
-  {
-    return _currentToken.getTokenType() == type;
-  }
+    private Object parseMember(HashMap map) {
+        //get the key
+        String key = _currentToken.getTokenValue();
+        Object val;
+        //now check that colon is separator
+        nextToken();
+        if (!match(COLON)) {
+            nextToken();
+            return error();
+        } else {
+            //now get value
+            nextToken();
+            val = parseValue();
+            //if error returned
+            if (val instanceof Error) {
+                return error();
+            }
+            map.put(key, val);
+        }
+        if (match(COMMA)) {
+            nextToken();
+            return parseMember(map);
+        } else if (match(RCURLY)) {
+            return map;
+        } else {
+            return error();
+        }
+    }
 
-  private Error error()
-  {
-    return new Error("Unexpected Token: " + _currentToken.toString());
-  }
+
+    public Object parseArray() {
+        ArrayList list = new ArrayList();
+        //for empty arrays
+        if (match(RSQUARE)) {
+            nextToken();
+            return list;
+        }
+        int rBrace = 0;
+        while (!match(RSQUARE) && !match(EOF)) {
+            //first get item
+                Object val = parseValue();
+                //if not comma, add to list since
+                if (val == null || !val.equals(",")) {
+                    list.add(val);
+                } else {
+                    //return error if not valid, already on next token
+                    //so don't need to switch index
+                    error();
+                    return new ArrayList();
+                }
+                //if a comma, move to next token
+                if (match(RSQUARE)) {
+                    rBrace = 1;
+                } else if (match(COMMA)) {
+                    nextToken();
+                } else {
+                    nextToken();
+                    return error();
+                }
+
+        }
+        nextToken();
+        if (rBrace != 1) {
+            return error();
+        }
+        return list;
+    }
+
+    //=================================================================================
+    //  Tokenizer helpers
+    //=================================================================================
+
+    private void nextToken() {
+        _currentToken = _tokenizer.next();
+    }
+
+    private boolean match(TokenType type) {
+        return _currentToken.getTokenType() == type;
+    }
+
+    private Error error() {
+        return new Error("Unexpected Token: " + _currentToken.toString());
+    }
+
+    private boolean isExponent() {
+        String val = _currentToken.getTokenValue();
+        return val.contains(".") || val.contains("e") || val.contains("E");
+    }
 }
--- src/test/java/org/jschema/parser/ParserTest.java
+++ src/test/java/org/jschema/parser/ParserTest.java
@@ -13,211 +13,201 @@ import java.util.List;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
-public class ParserTest
-{
+public class ParserTest {
 
-  @Test
-  public void testParseObject()
-  {
-    assertEquals( new HashMap(), parse( "{}" ) );
-    HashMap map1 = new HashMap();
-    map1.put( "foo", "bar" );
-      assertEquals( map1, parse( "{\"foo\":\"bar\"}" ) );
-      HashMap map5=new HashMap();
-    map5.put("bar","baz");
-      ArrayList temp=new ArrayList();
-      temp.add(map1);
-      temp.add(map5);
-      //array of objects
-      assertEquals( temp, parse( "[{\"foo\":\"bar\"},{\"bar\":\"baz\"}]" ) );
-      HashMap map7=new HashMap();
-      //object with object value
-      map7.put("nested",map1);
-      assertEquals(map7, parse( "{\"nested\":{\"foo\":\"bar\"}}" ));
-    HashMap map2 = new HashMap();
-      HashMap map3=new HashMap();
-      ArrayList testArray=new ArrayList();
-      testArray.add(1);
-      testArray.add(2);
-      testArray.add(3);
-    map2.put( "foo2", testArray );
-    assertEquals( map2, parse( "{\"foo2\":[1,2,3]}" ) );
-      //Array of array of objects and object
-      HashMap map4=new HashMap();
-      ArrayList testArray2=new ArrayList();
-      testArray2.add(temp);
-      testArray2.add(map2);
-      map4.put("array",testArray2);
-      assertEquals( map4, parse( "{\"array\":[[{\"foo\":\"bar\"},{\"bar\":\"baz\"}],{\"foo2\":[1,2,3]}]}"));
-      //TODO add more
-      //object with object as value
-      HashMap map6=new HashMap();
-      map6.put("object",map2);
-      assertEquals(map6,parse("{\"object\":{\"foo2\":[1,2,3]}}"));
+    @Test
+    public void testParseObject() {
+        assertEquals(new HashMap(), parse("{}"));
+        HashMap map1 = new HashMap();
+        map1.put("foo", "bar");
+        assertEquals(map1, parse("{\"foo\":\"bar\"}"));
+        HashMap map5 = new HashMap();
+        map5.put("bar", "baz");
+        ArrayList temp = new ArrayList();
+        temp.add(map1);
+        temp.add(map5);
+        //array of objects
+        assertEquals(temp, parse("[{\"foo\":\"bar\"},{\"bar\":\"baz\"}]"));
+        HashMap map7 = new HashMap();
+        //object with object value
+        map7.put("nested", map1);
+        assertEquals(map7, parse("{\"nested\":{\"foo\":\"bar\"}}"));
+        HashMap map2 = new HashMap();
+        HashMap map3 = new HashMap();
+        ArrayList testArray = new ArrayList();
+        testArray.add(1);
+        testArray.add(2);
+        testArray.add(3);
+        map2.put("foo2", testArray);
+        assertEquals(map2, parse("{\"foo2\":[1,2,3]}"));
+        //Array of array of objects and object
+        HashMap map4 = new HashMap();
+        ArrayList testArray2 = new ArrayList();
+        testArray2.add(temp);
+        testArray2.add(map2);
+        map4.put("array", testArray2);
+        assertEquals(map4, parse("{\"array\":[[{\"foo\":\"bar\"},{\"bar\":\"baz\"}],{\"foo2\":[1,2,3]}]}"));
+        //TODO add more
+        //object with object as value
+        HashMap map6 = new HashMap();
+        map6.put("object", map2);
+        assertEquals(map6, parse("{\"object\":{\"foo2\":[1,2,3]}}"));
 
-    // empty
-    assertEquals( map(), parse( "{}" ) );
+        // empty
+        assertEquals(map(), parse("{}"));
 
-    // simple single
-    assertEquals( map( "foo", "bar" ), parse( "{\"foo\":\"bar\"}" ) );
-    assertEquals( map( "foo", 1 ), parse( "{\"foo\":1}" ) );
-    assertEquals( map( "foo", 1.1 ), parse( "{\"foo\":1.1}" ) );
-    assertEquals( map( "foo", true ), parse( "{\"foo\":true}" ) );
-    assertEquals( map( "foo", false ), parse( "{\"foo\":false}" ) );
-    assertEquals( map( "foo", null ), parse( "{\"foo\":null}" ) );
-    assertEquals( map( "foo", list() ), parse( "{\"foo\":[]}" ) );
+        // simple single
+        assertEquals(map("foo", "bar"), parse("{\"foo\":\"bar\"}"));
+        assertEquals(map("foo", 1), parse("{\"foo\":1}"));
+        assertEquals(map("foo", 1.1), parse("{\"foo\":1.1}"));
+        assertEquals(map("foo", true), parse("{\"foo\":true}"));
+        assertEquals(map("foo", false), parse("{\"foo\":false}"));
+        assertEquals(map("foo", null), parse("{\"foo\":null}"));
+        assertEquals(map("foo", list()), parse("{\"foo\":[]}"));
 
-    // complex single
-    assertEquals( map("foo", map( "foo", "bar" )), parse( "{\"foo\" : {\"foo\":\"bar\"}}" ) );
-    assertEquals( map("foo", list( "foo", "bar" )), parse( "{\"foo\" : [\"foo\", \"bar\"]}" ) );
+        // complex single
+        assertEquals(map("foo", map("foo", "bar")), parse("{\"foo\" : {\"foo\":\"bar\"}}"));
+        assertEquals(map("foo", list("foo", "bar")), parse("{\"foo\" : [\"foo\", \"bar\"]}"));
 
-    // simple multi
-    assertEquals( map( "foo", "bat", "doh", "rey" ), parse( "{\"foo\":\"bat\", \"doh\":\"rey\"}" ) );
-   assertEquals( map( "foo", "rey" ), parse( "{\"foo\":\"bar\", \"foo\":\"rey\"}" ) );
-  }
-
-
-  @Test
-  public void testParsePunc() {
-    assertEquals( ",", parse( "," ) );
-  }
-  @Test
-  public void testParseArray()
-  {
-    assertEquals( new ArrayList(), parse( "[]" ) );
-    assertEquals( Arrays.asList( "foo", "bar" ), parse( "[\"foo\", \"bar\"]" ) );
-    int[][] nestedArray = {{1,2,3},{4,5,6}};
-    ArrayList nestArray=new ArrayList();
-    ArrayList nest1= new ArrayList();
-    nest1.add(1);
-    nest1.add(2);
-    nest1.add(3);
-    ArrayList nest2= new ArrayList();
-    nest2.add(4);
-    nest2.add(5);
-    nest2.add(6);
-    nestArray.add(nest1);
-    nestArray.add(nest2);
-    assertEquals(Arrays.asList(1,2,3), parse( "[1,2,3]" ) );
-    assertEquals(nestArray, parse( "[[1,2,3],[4,5,6]]" ));
-      //assorted array
-      HashMap map1 = new HashMap();
-      map1.put( "foo", "bar" );
-      nestArray.add(map1);
-      assertEquals( nestArray, parse( "[[1,2,3],[4,5,6],{\"foo\":\"bar\"}]" ) );
-    assertEquals( list(), parse( "[]" ) );
-    assertEquals( list( "foo"), parse( "[\"foo\"]" ) );
-    assertEquals( list( "foo", "bar" ), parse( "[\"foo\", \"bar\"]" ) );
-    assertEquals( list( "string", 1, 1.1, map("foo", "bar"), list("doh"), true, false, null ),
-                  parse( "[\"string\", 1, 1.1, {\"foo\" : \"bar\"}, [\"doh\"], true, false, null]" ) );
-  }
-
-  @Test
-  public void testParseLiterals()
-  {
-    // strings
-    assertEquals( "", parse( "\"\"" ) );
-    assertEquals( "foo", parse( "\"foo\"" ) );
-    assertEquals( "foo\"bar", parse( "\"foo\\\"bar\"" ) );
-
-    // numbers
-    assertEquals( 0, parse( "0" ) );
-    assertEquals( 1, parse( "1" ) );
-    assertEquals( 123456789, parse( "123456789" ) );
-    assertEquals( -1, parse( "-1" ) );
-    assertEquals( -0, parse( "-0" ) );
-    assertEquals( -123456789, parse( "-123456789" ) );
-    assertEquals( 1.1, parse( "1.1" ) );
-    assertEquals( 123456789.1, parse( "123456789.1" ) );
-    assertEquals(123456.123456, parse("123456.123456"));
-    assertEquals( -1.1, parse( "-1.1" ) );
-    assertEquals(-123456789.1, parse("-123456789.1"));
-    assertEquals(-123456.123456, parse("-123456.123456"));
-    assertEquals( 1e1, parse( "1e1" ) );
-    assertEquals( 123456789e1, parse( "123456789e1" ) );
-    assertEquals( 1e+1, parse( "1e+1" ) );
-    assertEquals( 1e+1, parse( "1e+1" ) );
-    assertEquals( 1e-1, parse( "1e-1" ) );
-    assertEquals( 1E1, parse( "1E1" ) );
-    assertEquals( 1E+1, parse( "1E+1" ) );
-    assertEquals( 1E-1, parse( "1E-1" ) );
-
-    // literals
-    assertEquals( true, parse( "true" ) );
-    assertEquals( false, parse( "false" ) );
-    assertEquals( null, parse( "null" ) );
-  }
-
-
-  @Test
-  public void testErrors() {
-
-    // bad punctuation
-    assertTrue( parse( "{" ) instanceof Error);
-    assertTrue( parse( "}" ) instanceof Error);
-    assertTrue( parse( "}{" ) instanceof Error);
-    assertTrue( parse( "[" ) instanceof Error);
-    assertTrue( parse( "]" ) instanceof Error);
-    assertTrue( parse( "][" ) instanceof Error);
-    assertTrue( parse( "," ) instanceof Error);
-    assertTrue( parse( ":" ) instanceof Error);
-
-    // bad strings
-    assertTrue( parse( "\"" ) instanceof Error);
-    assertTrue( parse( "\"foo" ) instanceof Error);
-
-    // bad numbers
-    assertTrue( parse( ".1" ) instanceof Error);
-    assertTrue( parse( "-.1" ) instanceof Error);
-    assertTrue( parse( "e1" ) instanceof Error);
-    assertTrue( parse( "e+1" ) instanceof Error);
-    assertTrue( parse( "e-1" ) instanceof Error);
-    assertTrue( parse( "E1" ) instanceof Error);
-    assertTrue( parse( "E+1" ) instanceof Error);
-    assertTrue( parse( "E-1" ) instanceof Error);
-    assertTrue( parse( "1E.1" ) instanceof Error);
-
-    // bad objects
-    assertTrue( parse( "{\"foo\"}" ) instanceof Error);
-    assertTrue( parse( "{\"foo\":}" ) instanceof Error);
-    assertTrue( parse( "{\"foo\": badToken}" ) instanceof Error);
-
-    // bad arrays
-    assertTrue( parse( "[1" ) instanceof Error);
-    assertTrue( parse( "[1," ) instanceof Error);
-    assertTrue( parse( "[1,]" ) instanceof Error);
-    assertTrue( parse( "[1, badToken]" ) instanceof Error);
-    assertTrue( parse( "[1, [badToken]]" ) instanceof Error);
-
-    // bad literals
-    assertTrue( parse( "badToken" ) instanceof Error);
-    assertTrue( parse( "True" ) instanceof Error);
-    assertTrue( parse( "nil" ) instanceof Error);
-  }
-
-
-  //======================================================================
-  //  HELPERS
-  //======================================================================
-  private Object parse( String src )
-  {
-    return new Parser( src ).parse();
-  }
-
-  private List list(Object... listVals)
-  {
-    return Arrays.asList( listVals );
-  }
-
-  private HashMap map(Object... mapVals)
-  {
-    HashMap m = new HashMap();
-    Iterator it = Arrays.asList( mapVals ).iterator();
-    while(it.hasNext())
-    {
-      m.put( it.next(), it.next() );
+        // simple multi
+        assertEquals(map("foo", "bat", "doh", "rey"), parse("{\"foo\":\"bat\", \"doh\":\"rey\"}"));
+        assertEquals(map("foo", "rey"), parse("{\"foo\":\"bar\", \"foo\":\"rey\"}"));
     }
-    return m;
-  }
+
+
+    @Test
+    public void testParseArray() {
+        assertEquals(new ArrayList(), parse("[]"));
+        assertEquals(Arrays.asList("foo", "bar"), parse("[\"foo\", \"bar\"]"));
+        int[][] nestedArray = {{1, 2, 3}, {4, 5, 6}};
+        ArrayList nestArray = new ArrayList();
+        ArrayList nest1 = new ArrayList();
+        nest1.add(1);
+        nest1.add(2);
+        nest1.add(3);
+        ArrayList nest2 = new ArrayList();
+        nest2.add(4);
+        nest2.add(5);
+        nest2.add(6);
+        nestArray.add(nest1);
+        nestArray.add(nest2);
+        assertEquals(Arrays.asList(1, 2, 3), parse("[1,2,3]"));
+        assertEquals(nestArray, parse("[[1,2,3],[4,5,6]]"));
+        //assorted array
+        HashMap map1 = new HashMap();
+        map1.put("foo", "bar");
+        nestArray.add(map1);
+        assertEquals(nestArray, parse("[[1,2,3],[4,5,6],{\"foo\":\"bar\"}]"));
+        assertEquals(list(), parse("[]"));
+        assertEquals(list("foo"), parse("[\"foo\"]"));
+        assertEquals(list("foo", "bar"), parse("[\"foo\", \"bar\"]"));
+        assertEquals(list("string", 1, 1.1, map("foo", "bar"), list("doh"), true, false, null),
+                parse("[\"string\", 1, 1.1, {\"foo\" : \"bar\"}, [\"doh\"], true, false, null]"));
+    }
+
+    @Test
+    public void testParseLiterals() {
+        // strings
+        assertEquals("", parse("\"\""));
+        assertEquals("foo", parse("\"foo\""));
+        assertEquals("foo\"bar", parse("\"foo\\\"bar\""));
+
+        // numbers
+        assertEquals(0, parse("0"));
+        assertEquals(1, parse("1"));
+        assertEquals(123456789, parse("123456789"));
+        assertEquals(-1, parse("-1"));
+        assertEquals(-0, parse("-0"));
+        assertEquals(-123456789, parse("-123456789"));
+        assertEquals(1.1, parse("1.1"));
+        assertEquals(123456789.1, parse("123456789.1"));
+        assertEquals(123456.123456, parse("123456.123456"));
+        assertEquals(-1.1, parse("-1.1"));
+        assertEquals(-123456789.1, parse("-123456789.1"));
+        assertEquals(-123456.123456, parse("-123456.123456"));
+        assertEquals(1e1, parse("1e1"));
+        assertEquals(123456789e1, parse("123456789e1"));
+        assertEquals(1e+1, parse("1e+1"));
+        assertEquals(1e+1, parse("1e+1"));
+        assertEquals(1e-1, parse("1e-1"));
+        assertEquals(1E1, parse("1E1"));
+        assertEquals(1E+1, parse("1E+1"));
+        assertEquals(1E-1, parse("1E-1"));
+
+        // literals
+        assertEquals(true, parse("true"));
+        assertEquals(false, parse("false"));
+        assertEquals(null, parse("null"));
+    }
+
+
+    @Test
+    public void testErrors() {
+
+        // bad punctuation
+        assertTrue(parse("{") instanceof Error);
+        assertTrue(parse("}") instanceof Error);
+        assertTrue(parse("}{") instanceof Error);
+        assertTrue(parse("[") instanceof Error);
+        assertTrue(parse("]") instanceof Error);
+        assertTrue(parse("][") instanceof Error);
+        assertTrue(parse(",") instanceof Error);
+        assertTrue(parse(":") instanceof Error);
+
+        // bad strings
+        assertTrue(parse("\"") instanceof Error);
+        assertTrue(parse("\"foo") instanceof Error);
+        assertTrue(parse("foo\"") instanceof Error);
+
+        // bad numbers
+        assertTrue(parse(".1") instanceof Error);
+        assertTrue(parse("-.1") instanceof Error);
+        assertTrue(parse("e1") instanceof Error);
+        assertTrue(parse("e+1") instanceof Error);
+        assertTrue(parse("e-1") instanceof Error);
+        assertTrue(parse("E1") instanceof Error);
+        assertTrue(parse("E+1") instanceof Error);
+        assertTrue(parse("E-1") instanceof Error);
+        assertTrue(parse("1E.1") instanceof Error);
+
+        // bad objects
+        assertTrue(parse("{\"foo\"}") instanceof Error);
+        assertTrue(parse("{\"foo\":}") instanceof Error);
+        assertTrue(parse("{\"foo\": \"bar\"}}") instanceof Error);
+        assertTrue(parse("{\"foo\": badToken}") instanceof Error);
+
+        // bad arrays
+        assertTrue(parse("[1") instanceof Error);
+        assertTrue(parse("[1,") instanceof Error);
+        assertTrue(parse("[1,]") instanceof Error);
+        assertTrue(parse("[1, badToken]") instanceof Error);
+        assertTrue(parse("[1, [badToken]]") instanceof Error);
+
+        // bad literals
+        assertTrue(parse("badToken") instanceof Error);
+        assertTrue(parse("True") instanceof Error);
+        assertTrue(parse("nil") instanceof Error);
+    }
+
+
+    //======================================================================
+    //  HELPERS
+    //======================================================================
+    private Object parse(String src) {
+        return new Parser(src).parse();
+    }
+
+    private List list(Object... listVals) {
+        return Arrays.asList(listVals);
+    }
+
+    private HashMap map(Object... mapVals) {
+        HashMap m = new HashMap();
+        Iterator it = Arrays.asList(mapVals).iterator();
+        while (it.hasNext()) {
+            m.put(it.next(), it.next());
+        }
+        return m;
+    }
 
 }
