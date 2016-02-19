cba3397829d661094799fb3ac44b8e5d042e3ea0
Sachin Patel <me@gizmosachin.com>
2016-02-18 19:45:04 -0800

    Initial work on JSON → JSchema generator

--- src/main/java/org/jschema/generators/JSONToJSchemaRunner.java
+++ src/main/java/org/jschema/generators/JSONToJSchemaRunner.java
@@ -4,15 +4,27 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class JSONToJSchemaRunner
 {
-  public static void main( String[] args ) throws Exception
-  {
+  public static void main( String[] args ) throws Exception {
+    testEquals(jschemaString("{\"string\":\"String\",\"int\":20,\"boolean\":true}"), "{\"name\":\"@string\",\"age\":\"@int\",\"boolean\":\"@boolean\"}");
+  }
+
+  private static void testEquals(String first, String second) {
+    if (first.equals(second)) { return; }
+    System.out.println("Assertion failed: " + first + " does not equal " + second);
+  }
+
+  private static String jschemaString(String json) throws Exception {
+    return (String) executeJS("generateJSchemaStringFromJSON", json);
+  }
+
+  private static Object executeJS(String functionName, Object argument) throws Exception {
     ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     engine.eval(new FileReader("src/main/resources/js/json_to_jschema.js"));
     Invocable invocable = (Invocable) engine;
-    Object result = invocable.invokeFunction("jsonToJSchema", "{}");
-    System.out.println(result);
+    return invocable.invokeFunction(functionName, argument);
   }
 }
--- src/main/resources/js/json_to_jschema.js
+++ src/main/resources/js/json_to_jschema.js
@@ -12,6 +12,35 @@ function updateSchema(document, schema) {
   return schema
 }
 
-function printSchema(schema) {
-  return "{}"
+// JSON -> JSchema generator
+function generateJSchemaFromJSON(json) {
+    var jschema = JSON.parse(json);
+    for (key in jschema) {
+        jschema[key] = getType(jschema[key]);
+    }
+    return jschema;
+}
+
+function getType(value) {
+    var nativeJSType = typeof value;
+    if (nativeJSType == "string") {
+        return "@string";
+    } else if (nativeJSType == "number") {
+        if (value % 1 === 0) {
+            return "@int";
+        } else {
+            return "@number";
+        }
+    } else if (nativeJSType == "boolean") {
+        return "@boolean";
+    } else if (nativeJSType == "object") {
+        return "@object";
+    } else {
+        return "*";
+    }
+}
+
+// Debugging
+function generateJSchemaStringFromJSON(json) {
+    return JSON.stringify(generateJSchemaFromJSON(json));
 }
