2ca12a1206f7ae7e2f68e9731964bb3b9f04ad1c
RemingtonMaxwell <remingtonmaxwell@att.net>
2016-02-22 08:05:24 -0800

    Implementing parser with Carson's suggestions

--- src/main/java/org/jschema/parser/Parser.java
+++ src/main/java/org/jschema/parser/Parser.java
@@ -36,20 +36,17 @@ public class Parser {
             nextToken();
             return parseObject();
         }
-
         // parse arrays
         if (match(LSQUARE)) {
             nextToken();
             return parseArray();
         }
-
         // parse literals (e.g. true, false, strings, numbers)
         if (match(STRING)) {
             String tokenValue = _currentToken.getTokenValue();
             nextToken();
             return tokenValue;
         }
-
         if (match(TRUE)) {
             nextToken();
             return true;
@@ -62,7 +59,6 @@ public class Parser {
             nextToken();
             return null;
         }
-
         if (match(NUMBER)) {
             double num = _currentToken.getTokenNumberValue();
             //check if is an int
@@ -72,115 +68,85 @@ public class Parser {
             }
             nextToken();
             return num;
-
         }
         if (match(ERROR)) {
             return error();
         }
-
-
         return error();
     }
 
-
     public Object parseObject() {
-        //pass the map into parseMember to populate
         HashMap<String, Object> map = new HashMap<>();
-        //for empty object
-        if (match(RCURLY)) {
-            nextToken();
-            return map;
-        }
-        int rBrace = 0;
-        //keep going if there is a comma
-        while (!match(RCURLY) && !match(EOF)) {
-            try {
-                map = (HashMap<String, Object>) parseMember(map);
-
-                if (!match(RCURLY) || match(COMMA)) {
-                    nextToken();
-                }
-                if (match(RCURLY)) {
-                    rBrace = 1;
-                }
-                //if a map isn't returned, an error must be returned
-            } catch (ClassCastException e) {
-                return error();
+        Object val;
+        if(match(STRING)){
+            val=parseMember(map);
+            if(val instanceof Error){
+                return val;
+            }else{
+                map=(HashMap<String, Object>)val;
             }
         }
-        nextToken();
-        //if no matching right brace, return error
-        if (rBrace != 1) {
+        while (match(COMMA)) {
+            nextToken();
+                val=parseMember(map);
+                if(val instanceof Error){
+                    return val;
+                }else{
+                    map=(HashMap<String, Object>)val;
+                }
+        }
+        if (!match(RCURLY)) {
+            nextToken();
             return error();
         }
+        nextToken();
         return map;
     }
 
     private Object parseMember(HashMap map) {
-        //get the key
-        String key = _currentToken.getTokenValue();
-        Object val;
-        //now check that colon is separator
-        nextToken();
-        if (!match(COLON)) {
+        String key="";
+        if(match(STRING)) {
+            key = _currentToken.getTokenValue();
             nextToken();
-            return error();
-        } else {
-            //now get value
-            nextToken();
-            val = parseValue();
-            //if error returned
-            if (val instanceof Error) {
-                return error();
+            if (match(COLON)) {
+                nextToken();
+                Object val = parseValue();
+                if (val instanceof Error) {
+                    return error();
+                }
+                map.put(key, val);
+                return map;
             }
-            map.put(key, val);
         }
-        if (match(COMMA)) {
-            nextToken();
-            return parseMember(map);
-        } else if (match(RCURLY)) {
-            return map;
-        } else {
             return error();
-        }
     }
 
 
     public Object parseArray() {
         ArrayList list = new ArrayList();
+        Object val;
         //for empty arrays
-        if (match(RSQUARE)) {
+        if (!match(RSQUARE)) {
+            val=parseValue();
+            if (val instanceof Error) {
+                return error();
+            } else {
+                list.add(val);
+            }
+        }
+        while (match(COMMA)) {
             nextToken();
-            return list;
+            val = parseValue();
+            if (val instanceof Error) {
+                return error();
+            } else {
+                list.add(val);
+            }
         }
-        int rBrace = 0;
-        while (!match(RSQUARE) && !match(EOF)) {
-            //first get item
-                Object val = parseValue();
-                //if not comma, add to list since
-                if (val == null || !val.equals(",")) {
-                    list.add(val);
-                } else {
-                    //return error if not valid, already on next token
-                    //so don't need to switch index
-                    error();
-                    return new ArrayList();
-                }
-                //if a comma, move to next token
-                if (match(RSQUARE)) {
-                    rBrace = 1;
-                } else if (match(COMMA)) {
-                    nextToken();
-                } else {
-                    nextToken();
-                    return error();
-                }
-
-        }
-        nextToken();
-        if (rBrace != 1) {
+        if (!match (RSQUARE)) {
             return error();
         }
+        nextToken();
         return list;
     }
 
