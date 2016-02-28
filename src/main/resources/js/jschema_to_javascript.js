73c93f868e8c1c037de9fa331b417a4064c547ac
Ashley Rocha <contact@ash-arr.net>
2016-02-28 05:46:06 -0800

    type checking for string and boolean

--- src/main/resources/js/jschema_to_javascript.js
+++ src/main/resources/js/jschema_to_javascript.js
@@ -3,23 +3,26 @@
   JSON documents that satisfy a given jSchema
 */
 
+// TODO: array, enum, struct, nested schema
 function generateJavascriptForJSchema(jSchema, className) {
   var schema = JSON.parse(jSchema);
   var generatedSource =
       "var " + className + " = {\n" +
-        "parse: function(jsonData){\n";
+      "parse: function(jsonData){\n";
 
   for(var key in schema){
     if (schema.hasOwnProperty(key)){
       generatedSource +=
         "var _" + key + ";\n" +
-        "return {\n" +
-          "get " + key + "(){\n" +
-            "return _" + key + ";\n" +
-          "},\n" +
-          "set " + key + "(value){\n" +  /* TODO: type check */
-            "return _" + key + " = value;\n" +
-          "}" +
+        "var _type = '" + schema[key] + "';\n" +
+        "var validateType = function(elem) {\n" +
+          generateValidator(schema[key]) +
+        "};\n" +
+        "return {\n  " +
+          generateGetter(key) +
+          ",\n" +
+          generateSetter(key) +
+          "\n"  +
         "};\n"
     }
   }
@@ -27,4 +30,35 @@ function generateJavascriptForJSchema(jSchema, className) {
   generatedSource +=
       "}};";
   return generatedSource;
+}
+
+// https://toddmotto.com/understanding-javascript-types-and-reliable-type-checking/
+function generateIsJSType(type){
+    return "return Object.prototype.toString.call(elem).slice(8, -1) === '" + type + "';";
+}
+
+// TODO: date, uri, int, number
+function generateValidator(type){
+    switch(type){
+        case "@string" :
+            return generateIsJSType('String');
+        case "@boolean" :
+            return generateIsJSType('Boolean');
+        default:
+            return "return True";
+    }
+}
+function generateGetter(key){
+  return "get " + key + "(){\n" +
+           "return _" + key + ";\n" +
+         "}";
+}
+
+function generateSetter(key){
+  return "set " + key + "(value){\n" +
+         "if (validateType(value)){\n" +
+         "return _" + key + " = value;\n" +
+         "}\n" +
+         "return console.log(value + \" does not conform to \" + _type);\n" +
+         "}";
 }
