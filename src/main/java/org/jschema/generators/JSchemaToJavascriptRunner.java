b170d305fa135f413547186035bb56d7cf8db7c5
Ashley Rocha <contact@ash-arr.net>
2016-03-07 01:36:18 -0800

    minor tweaks

--- src/main/java/org/jschema/generators/JSchemaToJavascriptRunner.java
+++ src/main/java/org/jschema/generators/JSchemaToJavascriptRunner.java
@@ -10,7 +10,7 @@ public class JSchemaToJavascriptRunner
   public static void main( String[] args ) throws Exception
   {
     String test1 = "{ \"name\" : \"@string\", \"age\" : \"@int\", \"birthday\" : \"@date\", \"website\" : \"@uri\", \"student\" : \"@boolean\", \"favorite_number\":\"@number\" }";
-    String test2 = "{ \"else\" : \"@string\", \"age\" : \"@int\"}";
+    String test2 = "{ \"name\" : \"@string\", \"age\" : \"@int\"}";
     ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     engine.eval(new FileReader("src/main/resources/js/jschema_to_javascript.js"));
     Invocable invocable = (Invocable) engine;
--- src/main/resources/js/jschema_to_javascript.js
+++ src/main/resources/js/jschema_to_javascript.js
@@ -1,17 +1,17 @@
-/*
-  This javascript file provides functionality for generating java source code for working with
-  JSON documents that satisfy a given jSchema
-*/
+/*/*
+ This javascript file provides functionality for generating java source code for working with
+ JSON documents that satisfy a given jSchema
+ */
 
 // TODO: array, enum, struct
 function generateJavascriptForJSchema(jSchema, className) {
-  var parseFunction =  "parse: function(jsonData){" +
-                       "var json;" +
-                       "if(typeof jsonData != 'undefined'){" +
-                       "try{var json = JSON.parse(jsonData);" +
-                       "}catch(e){" +
-                       "return \"Invalid JSON format\";" +
-                       "}return Object.assign(json, this.create());}}"
+  var parseFunction = "parse: function(jsonData){" +
+                      "var json;" +
+                      "if(typeof jsonData != 'undefined'){" +
+                      "try{var json = JSON.parse(jsonData);" +
+                      "}catch(e){" +
+                      "return \"Invalid JSON format\";" +
+                      "}return Object.assign(json, this.create());}}"
 
   try{
     var schema = JSON.parse(jSchema);
@@ -20,9 +20,9 @@ function generateJavascriptForJSchema(jSchema, className) {
   }
 
   return  "var " + className + " = {" +
-          generateCreate(schema) +
-          parseFunction +
-          "};";
+      generateCreate(schema) +
+      parseFunction +
+      "};";
 }
 
 function generateCreate(schema){
@@ -34,53 +34,51 @@ function generateCreate(schema){
     }
   }
   return  "create: function(){" +
-          "return{" +
-          "validate : function(){" +
-          "var validators = {};" +
-          "var msg = \"\";" +
-          generatedSetters +
-          "for(var key in validators){" +
-          "if (this[key]){" +
-          "msg += validators[key](this[key]);" +
-          "}}"+
-          "if(msg === \"\") return \"Valid\";" +
-          "return msg;}," +
-          "toJSON : function(){" +
-          "var toJson = {};" +
-          "for (var key in this){\n" +
-          "if (this.hasOwnProperty(key) && Object.prototype.toString.call(this[key]).slice(8, -1) !== 'Function') {\n" +
-          "toJson[key] = this[key];\n" +
-          "}}return toJson;}};},\n"
+      "return{" +
+      "validate: function(){" +
+      "var validators = {};" +
+      "var msg = \"\";" +
+      generatedSetters +
+      "for(var key in validators){" +
+      "if(this[key]){" +
+      "msg += validators[key](this[key]);" +
+      "}}"+
+      "if(msg === \"\"){return \"Valid\"};" +
+      "return msg;}," +
+      "toJSON: function(){" +
+      "var toJson = {};" +
+      "for (var key in this){\n" +
+      "if (this.hasOwnProperty(key) && Object.prototype.toString.call(this[key]).slice(8, -1) !== 'Function') {\n" +
+      "toJson[key] = this[key];\n" +
+      "}}return toJson;}};},\n"
 }
 
 function generateValidator(type){
   switch(type){
-  case "@string" :
-    // https://toddmotto.com/understanding-javascript-types-and-reliable-type-checking/
-    return "Object.prototype.toString.call(value).slice(8, -1) === 'String'";
-  case "@boolean" :
-    return "Object.prototype.toString.call(value).slice(8, -1) === 'Boolean'";
-  case "@date" :
-    return "!isNaN(Date.parse(value))";
-  case "@uri" :
-    // json_to_schema.js
-    return " /^(?:(?:(?:https?|ftp):)?\\/\\/)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})).?)(?::\\d{2,5})?(?:[/?#]\\S*)?$/i.test( value )"
-  case "@int" :
-    return "Number.isInteger(value)";
-  case "@number" :
-    return "!Number.isNaN(value)";
-  default: // wildcard
-    return "True";
+    case "@string" :
+      // https://toddmotto.com/understanding-javascript-types-and-reliable-type-checking/
+      return "Object.prototype.toString.call(value).slice(8, -1) === 'String'";
+    case "@boolean" :
+      return "Object.prototype.toString.call(value).slice(8, -1) === 'Boolean'";
+    case "@date" :
+      return "!isNaN(Date.parse(value))";
+    case "@uri" :
+      // json_to_schema.js
+      return " /^(?:(?:(?:https?|ftp):)?\\/\\/)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})).?)(?::\\d{2,5})?(?:[/?#]\\S*)?$/i.test( value )"
+    case "@int" :
+      return "Number.isInteger(value)";
+    case "@number" :
+      return "!Number.isNaN(value)";
+    default: // wildcard
+      return "True";
   }
 }
 
 function generateSetter(key, type) {
   return "validators[\"" + key + "\"] = function(value){\n" +
-    "if (" + generateValidator(type) + "){\n" +
-    "this." + key + " = value;\n" +
-    "return \"\"\n" +
-    "} else{\n" +
-    "return \"" + key + "=\" + value + \" does not conform to " + type + "\\n\";\n" +
-    "}return;\n" +
-    "};";
+      "if(" + generateValidator(type) + "){\n" +
+      "this." + key + " = value;\n" +
+      "return \"\"\n" +
+      "}return \"" + key + "=\" + value + \" does not conform to " + type + "\\n\";\n" +
+      "};";
 }
