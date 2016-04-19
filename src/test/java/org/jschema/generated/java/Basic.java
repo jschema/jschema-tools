64046d0bf646d5edd6ad659272bc7145ca8f158c
mstircha <mstircha@ucsc.edu>
2016-04-19 13:36:28 -0700

    rewrite in progress, core types taken care of

--- src/test/java/org/jschema/generated/java/Basic.java
+++ src/test/java/org/jschema/generated/java/Basic.java
@@ -1,11 +1,11 @@
 package org.jschema.generated.java;
-import java.util.Map;
+import java.util.*;
 public class Basic{
-  private Map<String, Object> _fields;
+  private Map<String, Object> _fields = new HashMap<String, Object>();
   public String toJSON(){return _fields.toString();}
-  public Object getname(){return _fields.get("name");}
-  public Object getage(){return _fields.get("age");}
+  public String getname(){return (String) _fields.get("name");}
   public void setname(Object name){_fields.put("name", name);}
+  public int getage(){return (int) _fields.get("age");}
   public void setage(Object age){_fields.put("age", age);}
 
 }
--- src/test/java/org/jschema/generated/java/Contact.java
+++ src/test/java/org/jschema/generated/java/Contact.java
@@ -1,17 +1,19 @@
 package org.jschema.generated.java;
-import java.util.Map;
+import java.util.*;
 public class Contact{
-  private Map<String, Object> _fields;
+  private Map<String, Object> _fields = new HashMap<String, Object>();
   public String toJSON(){return _fields.toString();}
-  public Object getfirst_name(){return _fields.get("first_name");}
-  public Object getlast_name(){return _fields.get("last_name");}
-  public Object getage(){return _fields.get("age");}
-  public Object gettype(){return _fields.get("type");}
-  public Object getemails(){return _fields.get("emails");}
+  public String getfirst_name(){return (String) _fields.get("first_name");}
   public void setfirst_name(Object first_name){_fields.put("first_name", first_name);}
+  public String getlast_name(){return (String) _fields.get("last_name");}
   public void setlast_name(Object last_name){_fields.put("last_name", last_name);}
+  public int getage(){return (int) _fields.get("age");}
   public void setage(Object age){_fields.put("age", age);}
+  public ARRAY gettype(){return (ARRAY) _fields.get("type");}
   public void settype(Object type){_fields.put("type", type);}
+  public ARRAY getemails(){return (ARRAY) _fields.get("emails");}
   public void setemails(Object emails){_fields.put("emails", emails);}
+  public OBJECT getobject(){return (OBJECT) _fields.get("object");}
+  public void setobject(Object object){_fields.put("object", object);}
 
 }
